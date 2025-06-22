bfef7c59ed9ceef3aaac791b08fd6ba4c8a85012
nextcode1 <stargateblk@gmail.com>
2025-06-22 13:57:20 +0200

    add chat message display and time formatting utility

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-import assets from "../assets/assets";
+import React, { useEffect, useRef } from "react";
+import assets, { messagesDummyData } from "../assets/assets";
+import { messageTimeFormatter } from "../lib/utils";
 
 const Chat = ({ selectedUser, setSelectedUser }) => {
+  const scrollEnd = useRef();
+
+  useEffect(() => {
+    if (scrollEnd.current) {
+      scrollEnd.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
   return selectedUser ? (
     <div className="h-full overflow-scroll relative backdrop-blur-lg">
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
@@ -25,6 +34,51 @@ const Chat = ({ selectedUser, setSelectedUser }) => {
           alt="Help"
           className="max-md:hidden max-w-5"
         />
+      </div>
+      {/* Chat messages area */}
+      <div className=" flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
+        {messagesDummyData.map((message, index) => (
+          <div
+            key={index}
+            className={`flex items-end gap-2 justify-end ${
+              message.senderId !== "680f50e4f10f3cd28382ecf9" &&
+              "flex-row-reverse"
+            }`}
+          >
+            {message.image ? (
+              <img
+                src={message.image}
+                alt=""
+                className="max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8"
+              />
+            ) : (
+              <p
+                className={`p-2 max-w-[200px] md:text-small font-light rounded-lb mb-8 break-all bg-violet-500/30 text-white ${
+                  message.senderId === "680f50e4f10f3cd28382ecf9"
+                    ? "rounded-br-none"
+                    : "rounded-bl-none"
+                }`}
+              >
+                {message.text}
+              </p>
+            )}
+            <div className="text-center text-xs">
+              <img
+                src={
+                  message.senderId === "680f50e4f10f3cd28382ecf9"
+                    ? assets.avatar_icon
+                    : selectedUser?.profilePic
+                }
+                alt="Avatar"
+                className="w-7 rounded-full"
+              />
+              <p className="text-gray-500">
+                {messageTimeFormatter(message.createdAt)}
+              </p>
+            </div>
+          </div>
+        ))}
+        <div ref={scrollEnd}></div>
       </div>
     </div>
   ) : (
