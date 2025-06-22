cc044efda2a0410f7d1152918877e1761c5919df
nextcode1 <stargateblk@gmail.com>
2025-06-23 00:59:32 +0200

    Add database connection and user model implementation

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import connectDB from "./lib/db.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -10,10 +11,12 @@ app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 app.use(express.json());
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 app.get("/", (req, res) => {
   res.send("Welcome to the server!");
+});
+
+await connectDB();
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
